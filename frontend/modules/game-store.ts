e3f31d16a80b9c6bbc16828ff802821f4a98e099
electrovir <electrovir@users.noreply.github.com>
2020-05-19 22:22:45 -0600

    add minion movement action
    create user on initial game state

--- frontend/modules/game-store.ts
+++ frontend/modules/game-store.ts
@@ -1,38 +1,45 @@
 import {createObjectStore} from "reduxular";
 import {GameAction} from "../../types/game-action";
 import {GameState} from "../../types/game-state";
-import {Player} from "../../types/player";
 import {getRandomString} from "./string";
 import {generateRandomColorString} from "./color";
 
-// placeholder until user can pick their own color and name
-export function generateRandomUserPlayer(): Player {
 
+function createInitialGameState(): GameState {
+    const user = {
+        color: generateRandomColorString(),
+        name: 'empty name',
+        id: getRandomString(),
+    };
+
+    const firstUserMinion = {
+        minionId: getRandomString(),
+        playerId: user.id,
+        x: 0,
+        y: 0,
+    }
 
     return {
-        color: generateRandomColorString(),
-        name: '',
-        id: getRandomString(),
+        minions: {
+            [firstUserMinion.minionId]: firstUserMinion
+        },
+        user,
+        players: {[user.id]: user},
     };
 }
 
-export const emptyGameState: Readonly<GameState> = {
-    minions: [],
-    user: undefined,
-    players: {},
-};
 
-export const GameStore = createObjectStore<Readonly<GameState>, Readonly<GameAction>>(emptyGameState, () => {}, gameStoreReducer);
+export const GameStore = createObjectStore<Readonly<GameState>, Readonly<GameAction>>(createInitialGameState(), () => {}, gameStoreReducer);
 
 function gameStoreReducer(state: Readonly<GameState>, action: Readonly<GameAction>): Readonly<GameState> {
     switch (action.type) {
         case 'ADD_MINION':
             return {
                 ...state,
-                minions: [
+                minions: {
                     ...state.minions,
-                    action.minion
-                ],
+                    [action.minion.minionId]: action.minion,
+                },
             };
         case 'ADD_PLAYER':
             if (state.players.hasOwnProperty(action.player.id)) {
@@ -45,20 +52,38 @@ function gameStoreReducer(state: Readonly<GameState>, action: Readonly<GameActio
                     [action.player.id]: action.player
                 },
             };
-        case 'CREATE_USER':
-            const user = generateRandomUserPlayer();
+
+        case 'MOVE_MINION':
+            const minion = state.minions[action.minionId];
+            const nextCoords = {
+                x: minion.x,
+                y: minion.y,
+            };
+
+            switch (action.movementDirection) {
+                case 'up':
+                    nextCoords.y += 10;
+                    break;
+                case 'down':
+                    nextCoords.y -= 10;
+                    break;
+                case 'right':
+                    nextCoords.x += 10;
+                    break;
+                case 'left':
+                    nextCoords.x -= 10;
+                    break;
+            }
+
             return {
                 ...state,
-                user,
-                minions: [
+                minions: {
                     ...state.minions,
-                    // add a new minion for the player
-                    {
-                        x: 0,
-                        y: 0,
-                        playerId: user.id,
-                    },
-                ],
+                    [minion.minionId]: {
+                        ...minion,
+                        ...nextCoords,
+                    }
+                },
             };
     }
 }
--- types/minion.d.ts
+++ types/minion.d.ts
@@ -2,4 +2,7 @@ export type Minion = {
     readonly x: number,
     readonly y: number,
     readonly playerId: string,
+    readonly minionId: string,
 };
+
+export type Minions = {[minionId: string]: Minion};
